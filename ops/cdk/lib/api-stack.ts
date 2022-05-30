b534b924c88122dd5d4029b2f4c66776660ed978
Adam Quigley <adamjohnquigley@gmail.com>
2022-05-30 17:06:03 +1000

    wip

--- ops/cdk/lib/api-stack.ts
+++ ops/cdk/lib/api-stack.ts
@@ -1,5 +1,5 @@
 import { Duration, Stack, StackProps } from "aws-cdk-lib";
-import { LambdaIntegration, RestApi } from "aws-cdk-lib/aws-apigateway";
+import { LambdaIntegration } from "aws-cdk-lib/aws-apigateway";
 import { Code, Function, Runtime } from "aws-cdk-lib/aws-lambda";
 import { Construct } from "constructs";
 
@@ -7,26 +7,29 @@ const LAMBDA_RUNTIME = Runtime.GO_1_X;
 const LAMBDA_DEFAULT_TIMEOUT = Duration.seconds(15);
 const LAMBDA_DEFAULT_MEMORY = 512; // megabytes
 const CODE_DIST = "../../dist";
+const OPENAPI_SPEC = "../../openapi/api.yaml";
 
 export class ApiStack extends Stack {
   constructor(scope: Construct, id: string, props?: StackProps) {
     super(scope, id, props);
 
-    const backendLambda = new Function(this, "ApiLambdaBackend", {
+    const backendLambda = new Function(this, "LambdaBackend", {
       runtime: LAMBDA_RUNTIME,
+      functionName: "GoRestApiLambdaBackend", // this is important to reference in openapi spec
       handler: "api",
       code: Code.fromAsset(CODE_DIST),
       timeout: LAMBDA_DEFAULT_TIMEOUT,
       memorySize: LAMBDA_DEFAULT_MEMORY,
     });
 
-    const apigw = new RestApi(this, "RestApi", {
-      restApiName: "Go Rest API Serverless",
-      description: "A serverless REST API built with Golang"
-    });
+    const backendLambdaIntegration = new LambdaIntegration(backendLambda);
 
-    apigw.root.addProxy({
-      defaultIntegration: new LambdaIntegration(backendLambda),
-    })
+    // const apigw = new SpecRestApi(this, "RestApi", {
+    //   apiDefinition: ApiDefinition.fromAsset(OPENAPI_SPEC)
+    // });
+
+    // apigw.root.addProxy({
+    //   defaultIntegration: new LambdaIntegration(backendLambda),
+    // })
   }
 }
